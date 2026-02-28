0c4666e8252bcf6cf55a4ccf0486d43fccaaedf4
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-28 20:00:27 +0530

    updated

--- 3-redis.sh
+++ 3-redis.sh
@@ -19,6 +19,9 @@ VALIDATE $? "Installing redis"              #sed -i 's/127.0.0.1/0.0.0.0/g'
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e  '/protected-mode/ c protected-mode no' /etc/redis/redis.conf 
 VALIDATE $? "updating changes allowinfg remote connections"
 
-systemd_setup
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
+
 
 print_total_time
