dca764b09e423f451da026117c88646a389cfb1d
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-28 13:12:20 +0530

    updated

---
+++ common.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # full path
+LOGS_FILE="/var/log/shell-roboshop/$0.log" # or we can write it as $LOGS_FOLODER/$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi 
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N " | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2... $G SUCCESS $N " | tee -a $LOGS_FILE
+    fi
+}
+
