89e3b197c9c32a509ac10579262c031de9661572
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-01 10:44:23 +0530

    updated

--- 8-rabbitmq.sh
+++ 8-rabbitmq.sh
@@ -19,12 +19,11 @@ systemctl enable rabbitmq-server &>>$LOGS_FILE
 systemctl start rabbitmq-server
 VALIDATE $? "Enabled and started rabbitmq Server"
 
-if [ $? -ne 0 ]; then
-    rabbitmqctl add_user roboshop roboshop123
-    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-    VALIDATE $? "created user and given permissions"
-else
-    echo -e "User already exist... $Y SKIPPING $N"
-fi
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "created user and given permissions"
+
+
 
 print_total_time
